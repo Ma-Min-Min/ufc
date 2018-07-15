2a7a109cd739e643f5c48ed27161c736bf6ad7a9
Ma-Min-Min <minmin12697@gmail.com>
2018-07-16 01:29:25 +0630

    replace character win-myanmar instead of uni

--- for_w2uni/w2un.py
+++ for_w2uni/w2un.py
@@ -25,4 +25,18 @@ def replace(input)
     output = re.sub(u'\u0077', u'\u1010', output) # ta_win_pu
     output = re.sub(u'\u0078', u'\u1011', output) # hta_sin_htoo
     output = re.sub(u'\u0027', u'\u1012', output) # da_dwe
-    
+    output = re.sub(u'\u0022', u'\u1013', output) # da_out_chai
+    output = re.sub(u'[\u0045\u0065]', u'\u1014', output) # na_nge
+    output = re.sub(u'\u0079', u'\u1015', output) # pa_thout
+    output = re.sub(u'\u007a', u'\u1016', output) # pha_oo_htoke
+    output = re.sub(u'\u0041', u'\u1017', output) # ba_htet_chai
+    output = re.sub(u'\u0062', u'\u1018', output) # ba_gone
+    output = re.sub(u'\u0072', u'\u1019', output) # ma
+    output = re.sub(u'\u002c', u'\u101a', output)# ya_pa_lat
+    output = re.sub(u'[\u0026\u00bd]', u'\u101b', output)  # ya_gout
+    output = re.sub(u'\u0076', u'\u101c', output)  # la
+    output = re.sub(u'\u0030', u'\u101d', output)  # wa
+    output = re.sub(u'\u006f', u'\u101e', output)  # tha
+    output = re.sub(u'\u005b', u'\u101f', output)  # ha
+    output = re.sub(u'\u0056', u'\u1020', output)  # la_gyi
+    output = re.sub(u'\u0074', u'\u1021', output)  # aa
